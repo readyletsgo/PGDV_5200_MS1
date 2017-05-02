877457482b8d9d8592eb88c29ca9b01e0597fa82
readyletsgo <rich@rt80.net>
2017-05-02 17:28:04 -0400

    let's play pong

--- 21_pong-bouncing-ball/sketch.js
+++ 21_pong-bouncing-ball/sketch.js
@@ -2,6 +2,9 @@ var x = 0;
 var y = 0;
 var xSpeed = 0;
 var ySpeed = 0;
+var backgroundColor;
+
+  // var inside = color(204, 102, 0);
 
 function setup() {
   createCanvas(600, 600);
@@ -12,33 +15,33 @@ function setup() {
     y = random(10, height-80);
     xSpeed = random(5, 5);
     ySpeed = random(5, 5);
- 
+    changeColor();
 }
 
 function draw() {
-  background(128, 128, 0);
+  // background(128, 128, 0);
+  background(backgroundColor);
 
   x+= xSpeed;
   y+= ySpeed;
   
     // left wall
-    if (x <= 10)
-      xSpeed *= -1;
-    
-    // right wall 
-    if (x >= width-10)
-      xSpeed *= -1;
+    if (x <= 10 || x >= width-10){
+      xSpeed *= -1;      
+      changeColor();
+    }
+
 
     // top wall 
-    if (y <= 10)
+    if (y <= 10 || y >= height-10){
       ySpeed *= -1;
-
-    // bottom wall 
-    if (y >= height-10)
-      ySpeed *= -1;
-    
+      changeColor();
+    }
 
     // ball
     ellipse(x, y, 20, 20);
  
+}
+function changeColor(){
+  backgroundColor = color(random(255), random(255), 150);
 }
